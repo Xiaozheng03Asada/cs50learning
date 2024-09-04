c46bb54286e699445b7c11b049a5ecc73b578a57
Xiaozheng03Asada <xiaozhengsdu2022@mail.sdu.edu.cn>
2024-09-04 23:29:00 +0800

    睡前上传

--- week0/hello.py
+++ week0/hello.py
@@ -1,6 +1,8 @@
 # Ask the user for their name
-name = input("What's your name?")
+name = input("What's your name? ")
 
-# Print the name inputted
-print("hello,",name)
+# Remove whitespace from the str
+name = name.strip()
 
+# Print the output
+print(f"hello, {name}")
