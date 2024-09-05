3711928833ceaeea85a7a89ed44a4f48247403e2
Xiaozheng03Asada <xiaozhengsdu2022@mail.sdu.edu.cn>
2024-09-05 14:42:41 +0800

    week0课程完成

---
+++ week0/calculator.py
@@ -0,0 +1,46 @@
+def main():
+    x = float(input("What's x?"))
+    print("x squared is", f"{square(x):.2f}")
+
+def square(n):
+    return n * n
+
+main()
+
+
+# # Get the user's input
+# x = float(input("What's x?"))
+# y = float(input("What's y?"))
+
+# # Calculate the result
+# z = x / y
+
+# # # Calculate the result and round
+# # z = round(x / y,3)
+
+# # Print the result
+# print(f"{z:.2f}")
+
+
+
+
+# # # Create a rounded result
+# # z = round(x+y)
+
+# # # # Print the result
+# # # print(z)
+
+# # # Print the formatted result
+# # print(f"{z:,}")
+
+# # x = input("What's x?")
+# # y = input("What's y?")
+
+# # # z = x + y
+# # z = int(x) + int(y)
+
+# # print(z)
+
+# # x = int(input("What's x?"))
+# # y = int(input("What's y?"))
+# # print(x+y)
--- week0/hello.py
+++ week0/hello.py
@@ -1,8 +1,18 @@
-# Ask the user for their name
-name = input("What's your name? ")
+def main():
+    name = input("What's your name?")
+    hello(name)
+    hello()
 
-# Remove whitespace from the str
-name = name.strip()
 
-# Print the output
-print(f"hello, {name}")
+
+def hello(to="world"):
+    print("Hello,", to)
+
+main()
+
+
+# # Ask the user for their name, remove whitespace from the str and capitalize the first letter of each word
+# name = input("What's your name? ").strip().title()
+
+# # Print the output
+# print(f"hello, {name}")
